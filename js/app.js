40f69de0c500ae91cfa66bb5981c0cc059074670
Cuong Pham <cuongphamquoc91@gmail.com>
2016-06-15 17:39:20 +0700

    End day 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module("gemStore", []);
+(function(){
+	var gems = [
+	    { name: 'Azurite', price: 2.95 },
+	    { name: 'Bloodstone', price: 5.95 },
+	    { name: 'Zircon', price: 3.95 }
+	  ];
+	var app = angular.module("gemStore", []);
+	app.controller("StoreController", function(){
+		this.products = gems;
+	})
+})();
